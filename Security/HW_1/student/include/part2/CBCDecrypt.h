6c5d3c2790326b35a89a2ecc7bdaadf066f8921a
Ant <ant@cs.columbia.edu>
2018-10-14 16:20:02 -0400

    finished part 2. Was just copy paste

--- Security/HW_1/student/include/part2/CBCDecrypt.h
+++ Security/HW_1/student/include/part2/CBCDecrypt.h
@@ -2,6 +2,7 @@
 #define CBC_DECRYPT_H
 
 #include <string>
+#include <vector>
 
 class EncryptionOracle;
 
@@ -9,5 +10,8 @@ class CBCDecrypt {
 public:
 
   std::string grader_decrypt(EncryptionOracle &e);
+  /* Checks that the contents of the two are equal to the first n elements */
+  bool isNEqual(std::vector<uint8_t> &lhs, std::vector<uint8_t> &rhs, uint n);
+  void print_ciphertext(std::vector<uint8_t> ciphertext);
 };
 #endif
--- Security/HW_1/student/src/part1/ECBDecrypt.cpp
+++ Security/HW_1/student/src/part1/ECBDecrypt.cpp
@@ -44,11 +44,10 @@ std::string ECBDecrypt::grader_decrypt(EncryptionOracle &e) {
   uint blockSize = 16;
   uint8_t alphabet[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
                     'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
-                    '1','2','3','4','5','6','7','8','9','0',',','.','!',' ','$' };
+                    '1','2','3','4','5','6','7','8','9','0',',','.','!',' ','$', '\'' };
 
   std::string postfix;
 
-  int tmp = 0;
   while(true)
   {
 
@@ -94,7 +93,6 @@ std::string ECBDecrypt::grader_decrypt(EncryptionOracle &e) {
       */
     if(!found)
       break;
-    tmp++;
   }
 
   return postfix;
