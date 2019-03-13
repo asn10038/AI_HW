94e986e322873ee9372fae00ea903b72ba9d5883
Ant <ant@cs.columbia.edu>
2019-03-13 17:30:24 -0400

    added js page

--- UID/HW_8/bbHOF/static/bbHOF.js
+++ UID/HW_8/bbHOF/static/bbHOF.js
@@ -1,10 +1,55 @@
 //CONSTANTS
+var all_positions = ["Catcher",
+                     "Pitcher",
+                     "Designated Hitter",
+                     "Centerfielder",
+                     "Rightfielder",
+                     "Leftfielder",
+                     "First Baseman",
+                     "Second Baseman",
+                     "Third Baseman",
+                     "Shortstop",
+                     "Outfielder" ] 
+var inputChecks = [
+  $('#firstBasemanInput'),
+  $('#secondBasemanInput'),
+  $('#thirdBasemanInput'),
+  $('#DHInput'),
+  $('#RightfielderInput'),
+  $('#LeftfieldsInput'),
+  $('#CenterfielderInput'),
+  $('#PitcherInput'),
+  $('#CatcherInput')
+]
+
 
 //LOGIC
+function getPositions() {
+
+  for
+}
+
+function addPlayer() {
+  fullname = $('#fullnameInput').val();
+  born = $('#bornInput').val();
+  bio = $('#bioInput').val();
+  image_url = $('#imageURLInput').val();
+
+  rookie_status = $('#rookieStatusInput').val();
+  last_game = $('#lastGameInput').val();
+
+  positions = getPositions();
+  bats = $('#batsInput').val();
+  throws = $('#throwsInput').val();
+  war = $().val('#WARInput').val();
+
+}
 
 //LAYOUT
-function
+function install_handlers(){
+  $('#addPlayerBtn').click(addPlayer);
+}
 //MAIN
 $(document).ready(function(){
-  console.log('im here');
+  install_handlers();
 });
