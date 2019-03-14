f5788139ca77a58c1a33224b2c91e62aea023987
asn10038 <asn10038>
2019-03-14 15:15:27 -0400

    finished the add player page

--- UID/HW_8/bbHOF/server.py
+++ UID/HW_8/bbHOF/server.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 
 DATA_FILE = '../players2.json'
 PLAYER_DATA = json.load(open(DATA_FILE))
+current_id = 100
 
 POSITIONS = ["Catcher", "Pitcher", "Designated Hitter", "Centerfielder", "Rightfielder",
              "Leftfielder", "First Baseman", "Second Baseman", "Third Baseman", "Shortstop", "Outfielder" ]
@@ -24,7 +25,11 @@ def hello(name=None):
 
 @app.route('/add_item', methods=['GET', 'POST'])
 def add_item():
-	return render_template('add-item.html', positions = POSITIONS)
+    if request.method == 'POST':
+        new_id = add_player(request.get_json())
+        return jsonify(data={'link-to-view' : '/view_item/'+str(new_id)})
+
+    return render_template('add-item.html', positions=POSITIONS)
 
 @app.route('/search', methods=['GET', 'POST'])
 def search():
@@ -33,6 +38,14 @@ def search():
 @app.route('/view_item/<id>')
 def view_item(id=3):
 	return render_template('view_item.html', id=id)
+
+def add_player(player):
+    global current_id
+    player['id'] = current_id
+    current_id +=1
+    PLAYER_DATA.append(player)
+    return player['id']
+
 
 if __name__ == '__main__':
    app.run(debug = True)
--- UID/HW_8/bbHOF/static/bbHOF.js
+++ UID/HW_8/bbHOF/static/bbHOF.js
@@ -9,27 +9,87 @@ var all_positions = ["Catcher",
                      "Second Baseman",
                      "Third Baseman",
                      "Shortstop",
-                     "Outfielder" ] 
+                     "Outfielder" ]
 var inputChecks = [
-  $('#firstBasemanInput'),
-  $('#secondBasemanInput'),
-  $('#thirdBasemanInput'),
-  $('#DHInput'),
-  $('#RightfielderInput'),
-  $('#LeftfieldsInput'),
-  $('#CenterfielderInput'),
-  $('#PitcherInput'),
-  $('#CatcherInput')
+  '#CatcherInput',
+  '#PitcherInput',
+  '#DHInput',
+  '#CenterfielderInput',
+  '#RightfielderInput',
+  '#LeftfielderInput',
+  '#firstBasemanInput',
+  '#secondBasemanInput',
+  '#thirdBasemanInput',
+  '#ShortstopInput',
+  '#OutfielderInput'
 ]
 
+var generalInputs = [
+  '#fullnameInput',
+  '#bornInput',
+  '#bioInput',
+  '#imageURLInput',
+  '#rookieStatusInput',
+  '#lastGameInput',
+  '#batsInput',
+  '#throwsInput',
+  '#WARInput'
+]
 
 //LOGIC
-function getPositions() {
 
-  for
+function inputError() {
+  alert("input error")
+}
+function getPositions() {
+  positions = []
+  for (var i in inputChecks) {
+    if($(inputChecks[i]).is(':checked')){
+      positions.push(all_positions[i])
+    }
+  }
+  console.log(positions)
+}
+
+function safeInput(){
+  var positionChecked = false;
+  WARVal = $('#WARInput').val()
+  if( isNaN(WARVal) || !isFinite(WARVal)) {
+    console.log('war val not valid')
+    return false;
+  }
+
+  for(var i in generalInputs) {
+    if ($(generalInputs[i]).val() == ''){
+      console.log(generalInputs[i])
+      return false
+    }
+  }
+  for(var i in inputChecks) {
+    console.log(inputChecks[i])
+    if($(inputChecks[i]).is(':checked')) {
+      positionChecked = true;
+      console.log('position is checked')
+      break;
+    }
+  }
+  if (!positionChecked) {
+    console.log('no positions checked')
+    return false;
+  }
+  return true;
+}
+function clearInputs() {
+  for (var i in inputChecks)
+    $(inputChecks[i]).prop('checked', false)
+  for (var i in generalInputs)
+    $(generalInputs[i]).val('');
 }
 
 function addPlayer() {
+  if(!safeInput())
+    return inputError();
+
   fullname = $('#fullnameInput').val();
   born = $('#bornInput').val();
   bio = $('#bioInput').val();
@@ -41,12 +101,58 @@ function addPlayer() {
   positions = getPositions();
   bats = $('#batsInput').val();
   throws = $('#throwsInput').val();
-  war = $().val('#WARInput').val();
+  war = $('#WARInput').val();
 
+  player = {
+    'fullname' : fullname,
+    'born' : born,
+    'bio' : bio,
+    'image_url' : image_url,
+    'rookie_status' : rookie_status,
+    'last_game' : last_game,
+    'positions' : positions,
+    'bats' : bats,
+    'throws' : throws,
+    'WAR' : war
+  }
+  $.ajax({
+      type: "POST",
+      url: "add_item",
+      dataType : "json",
+      contentType: "application/json; charset=utf-8",
+      data : JSON.stringify(player),
+      success: function(result){
+        console.log(result['data'])
+
+        //reconfig autocomplete
+        newPlayerLink = result['data']['link-to-view'];
+        console.log(newPlayerLink)
+        $('#successAdd').show()
+        // clearInputs()
+      },
+      error: function(request, status, error){
+          $('#failAdd').show()
+          console.log("Error");
+          console.log(request);
+          console.log(status);
+          console.log(error);
+      }
+  });
+  return player
 }
 
 //LAYOUT
+
 function install_handlers(){
+  $('#successAdd').hide()
+  $('#failAdd').hide()
+  $("#closeSuccessAlert").click(function() {
+      $('#successAdd').hide()
+  })
+  $("#closeFailAlert").click(function() {
+      $('#failAdd').hide()
+  })
+
   $('#addPlayerBtn').click(addPlayer);
 }
 //MAIN
