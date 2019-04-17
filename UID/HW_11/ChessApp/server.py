9f0fb75faa2738382127a24f48c5e3955c8b6d62
asn10038 <asn10038>
2019-04-17 15:57:48 -0400

    working showing evaluation for moves

--- UID/HW_11/ChessApp/server.py
+++ UID/HW_11/ChessApp/server.py
@@ -5,26 +5,35 @@ from flask import Response, request, jsonify
 import json
 from pprint import pprint
 from flask import send_from_directory
+import chess
+import chess.pgn
 app = Flask(__name__)
 
+GAMES = {}
 
 @app.route('/')
 def hello_world():
    return 'Hello World'
 
-@app.route('/games')
-def games():
-    return render_template('view-game.html')
+@app.route('/games/<id>')
+def games(id=None):
+    pgn = GAMES[id]
+    return render_template('view-game.html', pgn=str(GAMES[id]) )
 
 @app.route('/hello/<name>')
 def hello(name=None):
     return render_template('hello.html', name=name)
 
-@app.route('/img/<path:path>')
+@app.route('/games/img/<path:path>')
 def send_img(path):
     return send_from_directory('static/img', path)
 
-
+def load_games():
+    global GAMES
+    with open('/home/ant/GRAD_HW/UID/HW_11/chess_media/RobertJamesFischer.pgn') as pgn:
+        for x in range(50):
+            GAMES[str(x)] = chess.pgn.read_game(pgn)
 
 if __name__ == '__main__':
+    load_games()
     app.run(debug = True)
--- UID/HW_11/ChessApp/static/chess-game.js
+++ UID/HW_11/ChessApp/static/chess-game.js
@@ -1,3 +1,44 @@
+//CONSTANTS
+CURRENT_MOVE = 0
+MOVES = []
+game2 = new Chess()
+RESPONSE = ""
+CURRENT_SCORE = ""
+
+// new code that hopefully won't fuck the old code
+function clean_pgn() {
+  pgn = $('#game-pgn').text()
+  splt = pgn.split('\n')
+  splt.shift()
+  splt.pop()
+  splt[0] = splt[0].trim()
+  return splt.join('\n')
+}
+
+function make_next_move() {
+  game2.move(MOVES[CURRENT_MOVE])
+  //board.position(game.fen())
+  CURRENT_MOVE++
+  board.position(game2.fen())
+}
+
+function score_next_move() {
+  stockfish.postMessage("position fen " + board.fen())
+  stockfish.postMessage("eval")
+  return 0
+}
+
+function parse_evaluation(event) {
+  resp = event.data
+  RESPONSE += resp
+  if(RESPONSE.indexOf("Total Evaluation") != -1) {
+    score = RESPONSE.substring(RESPONSE.indexOf("Total Evaluation"))
+    CURRENT_SCORE = score
+    RESPONSE = ""
+    $("#scratch").text(CURRENT_SCORE)
+  }
+}
+//randome game code
 var board,
   game = new Chess();
 
@@ -50,3 +91,27 @@ var cfg = {
   onSnapEnd: onSnapEnd
 };
 board = ChessBoard('board', cfg);
+
+
+
+
+// new code that hopefully doesn't break everything
+$(document).ready(function(){
+  //stockfish worker
+  stockfish = new Worker('../static/js/stockfish.js')
+  stockfish.onmessage = function(event) { parse_evaluation(event); }
+  stockfish.onmessageerror = function(event) { alert("THERE WAS AN ERROR")}
+  stockfish.onerror = function(event){alert("ERROR " + event.message)}
+  //game stuff
+  if(!game.load_pgn(clean_pgn())) {
+    alert("ERROR ON GAME LOAD")
+  }
+  else {
+    console.log("game loaded")
+  }
+  MOVES = game.history()
+  $('#next').click(function() {
+    make_next_move();
+    score = score_next_move();
+  })
+})
